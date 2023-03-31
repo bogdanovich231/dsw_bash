b3957bf5a25d59f3ced35f9062fd30913d7aebe7
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-03-31 22:19:40 +0200

    add command git

---
+++ command.sh
@@ -0,0 +1,27 @@
+## level 1
+  git commit -m ''
+  git branch 
+  git checkout
+  git merge 
+  git rebase 
+
+## level 2
+  git checkout HEAD~
+  git checkout main^
+  git branch -f main HEAD~3
+
+## level 3
+  git cherry-pick
+  git rebase -i
+
+## level 4
+  git rebase -i
+  git cherry-pick
+  git tag
+  git describe
+  
+## level 5
+  git rebase -i
+  git cherry-pick
+  git merge 
+  git checkout main^
