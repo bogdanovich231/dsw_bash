2986ec702c66b87abc6d47fe023938afddb9b1e0
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2023-04-28 16:17:10 +0200

    feat: update flags

--- script.sh
+++ script.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
-  date
-elif [ "$1" == "--logs" ]; then
-  if [ "$2" ]; then
-    n=$2
-  else
+if [ "$1" == "-d" ]; then
+  echo "Aktualna data i godzina: $(date)"
+  exit 0
+if [ "$1" == "-l" ]; then
+  if [ "$2" == "" ]; then
     n=100
+  else
+    n=$2
   fi
   for (( i=1; i<=$n; i++ )); do
     echo "log$i.txt created by script.sh on $(date)" > log$i.txt
   done
-elif [ "$1" == "--help" ]; then
+elif [ "$1" == "-h" ]; then
   echo "Usage: script.sh [option]"
   echo "Options:"
-  echo "  --date          display today's date"
-  echo "  --logs [n]      create n log files (default: 100)"
-  echo "  --help          display this help message"
+  echo "  -d        display today's date"
+  echo "  -l [n]      create n log files (default: 100)"
+  echo "  -h         display this help message"
 else
   echo "Invalid option. Use --help to display available options."
 fi
